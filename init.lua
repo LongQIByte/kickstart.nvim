2b211a45d5ca8957b31562779245e1085fab1eb1
longqi <tzfjobmail@gmail.com>
2025-10-27 15:49:16 +0800

    feat: 修改toggleterm配置为全屏终端窗口
    - 将direction从'float'改为'tab'，实现全屏终端
    - 移除float_opts配置，不再需要浮动窗口选项
    - 更新size配置为函数形式，支持不同方向的终端大小设置

--- init.lua
+++ init.lua
@@ -989,7 +989,13 @@ require('lazy').setup({
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
-      size = 20,
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 15
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
       open_mapping = [[<c-\>]],
       hide_numbers = true,
       shade_filetypes = {},
@@ -998,17 +1004,9 @@ require('lazy').setup({
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = 'float',
+      direction = 'tab',
       close_on_exit = true,
       shell = vim.o.shell,
-      float_opts = {
-        border = 'curved',
-        winblend = 0,
-        highlights = {
-          border = 'Normal',
-          background = 'Normal',
-        },
-      },
     },
   },
 
